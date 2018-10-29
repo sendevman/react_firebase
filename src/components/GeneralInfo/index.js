6849ff67e5de2662466f2ef945af80e59177db39
nativelocation <bluesky6460@mail.com>
2018-10-29 16:11:39 -0300

    Generalinfo

--- src/components/GeneralInfo/index.js
+++ src/components/GeneralInfo/index.js
@@ -1,8 +1,14 @@
-import TextField from '@material-ui/core/TextField';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import FormLabel from '@material-ui/core/FormLabel';
 import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
+import Tooltip from '@material-ui/core/Tooltip';
+import SaveIcon from '@material-ui/icons/Save';
+import RefreshIcon from '@material-ui/icons/Refresh';
 
 import InputEvent from 'components/InputEvent';
 
@@ -18,110 +24,119 @@ class GeneralInfo extends InputEvent {
       reigon: '',
       type: '',
       fileName: '',
+      floorId1: '',
+      floorId2: '',
+      autoUpdate: false,
+      dbID: '',
+      cpID: '',
+      subtype: '',
     };
   }
 
+  upload = () => {
+  }
+
   render() {
+    const {
+      name,
+      storeId,
+      city,
+      state,
+      reigon,
+      type,
+      fileName,
+      floorId1,
+      floorId2,
+      autoUpdate,
+      dbID,
+      cpID,
+      subtype,
+    } = this.state;
     return (
-      <Grid item xs={12} sm={6}>
-        <div className="border-shadow">
-          <Grid container>
-            <Grid item xs={12}>
-              <div className="sub-container">
-                <FormLabel component="legend">General Info</FormLabel>
-                <FormGroup>
-                  <TextField
-                    className="Text-Field"
-                    type="text"
-                    name="name"
-                    label="Name"
-                    value={this.state.name}
-                    onChange={e => this.handleInputChange(e, 'name')}
-                    required
-                    margin="normal"
-                  />
-                  <TextField
-                    className="Text-Field"
-                    type="text"
-                    name="storeId"
-                    label="Store ID"
-                    value={this.state.storeId}
-                    onChange={e => this.handleInputChange(e, 'storeId')}
-                    required
-                    margin="normal"
-                  />
-                  <TextField
-                    className="Text-Field"
-                    type="text"
-                    name="city"
-                    label="City"
-                    value={this.state.city}
-                    onChange={e => this.handleInputChange(e, 'city')}
-                    required
-                    margin="normal"
-                  />
-                  <TextField
-                    className="Text-Field"
-                    type="text"
-                    name="state"
-                    label="State"
-                    value={this.state.state}
-                    onChange={e => this.handleInputChange(e, 'state')}
-                    required
-                    margin="normal"
-                  />
-                  <TextField
-                    className="Text-Field"
-                    type="text"
-                    name="reigon"
-                    label="Reigon"
-                    value={this.state.reigon}
-                    onChange={e => this.handleInputChange(e, 'reigon')}
-                    required
-                    margin="normal"
-                  />
-                  <TextField
-                    className="Text-Field"
-                    type="text"
-                    name="type"
-                    label="Type"
-                    value={this.state.type}
-                    onChange={e => this.handleInputChange(e, 'type')}
-                    required
-                    margin="normal"
-                  />
-                </FormGroup>
-              </div>
-            </Grid>
-            <Grid item xs={12}>
-              <div className="sub-container">
-                <FormLabel component="legend">Floorplan Upload</FormLabel>
-                <FormGroup>
-                  <div className="d-flex">
-                    <TextField
-                      className="Text-Field w-50"
-                      type="text"
-                      name="fileName"
-                      label="File Name.png"
-                      value={this.state.fileName}
-                      onChange={e => this.handleInputChange(e, 'fileName')}
-                      required
-                      margin="normal"
+      <Grid item xs={12}>
+        <Card>
+          <CardContent className="left-border-orange">
+            <Grid container>
+              <Grid item xs={12} sm={6}>
+                <div className="sub-container">
+                  <FormLabel component="legend">General Info</FormLabel>
+                  <FormGroup>
+                    {this.subrender('name', 'Name', name)}
+                    {this.subrender('storeId', 'Store ID', storeId)}
+                    {this.subrender('dbID', 'DBID', dbID)}
+                    {this.subrender('cpID', 'CPID', cpID)}
+                    {this.subrender('city', 'City', city)}
+                    {this.subrender('state', 'State', state)}
+                    {this.subrender('reigon', 'Reigon', reigon)}
+                    {this.subrender('type', 'Type', type)}
+                    {this.subrender('subtype', 'Subtype', subtype)}
+                  </FormGroup>
+                </div>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <div className="sub-container">
+                  <FormLabel component="legend">Walkbase</FormLabel>
+                  <FormGroup>
+                    {this.subrender('floorId1', 'Floor ID1', floorId1)}
+                    {this.subrender('floorId2', 'Floor ID2', floorId2)}
+                  </FormGroup>
+                </div>
+                <div className="sub-container">
+                  <FormLabel component="legend" className="mt-block">Floorplan Upload</FormLabel>
+                  <FormGroup>
+                    <div className="d-flex">
+                      {this.subrender('fileName', 'File Name.png', fileName)}
+                      <Button
+                        className="btn-icon-text att-green margin-top-30 margin-left"
+                        variant="contained"
+                        size="small"
+                        onClick={this.upload}>
+                        Upload
+                      </Button>
+                    </div>
+                  </FormGroup>
+                </div>
+                <div className="sub-container">
+                  <FormLabel component="legend" className="mt-block">IPog Info</FormLabel>
+                  <FormGroup>
+                    <div className="mt-block default-text-color">Last Update</div>
+                    <FormControlLabel
+                      label="Auto Update"
+                      control={
+                        <Checkbox
+                          checked={autoUpdate}
+                          onChange={e => this.handleCheckChange(e, 'autoUpdate')}
+                          value="autoUpdate" />
+                      }
                     />
+                  </FormGroup>
+                </div>
+                <div className="buttons-box mt-block">
+                  <Tooltip title="Create Product" placement="top">
                     <Button
-                      className="Submit-Button"
+                      className="btn-icon-text att-orange margin-top margin-left"
                       variant="contained"
-                      size="medium"
-                      color="primary"
-                      onClick={() => this._signIn()}>
-                      Upload
+                      size="small"
+                      aria-label="Create"
+                      onClick={this.refresh}>
+                      <RefreshIcon />
                     </Button>
-                  </div>
-                </FormGroup>
-              </div>
+                  </Tooltip>
+                  <Tooltip title="Create Product" placement="top">
+                    <Button
+                      className="btn-icon-text att-orange margin-top margin-left"
+                      variant="contained"
+                      size="small"
+                      aria-label="Create"
+                      onClick={this.upload}>
+                      <SaveIcon />
+                    </Button>
+                  </Tooltip>
+                </div>
+              </Grid>
             </Grid>
-          </Grid>
-        </div>
+          </CardContent>
+        </Card>
       </Grid>
     );
   }
