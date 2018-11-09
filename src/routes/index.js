1caf5bb3303e7cda58b1adf1f5a33cdc55318d58
nativelocation <bluesky6460@mail.com>
2018-11-09 19:12:36 -0300

    Add battery, camera, color component, update router, update firebase redux, add assets

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,6 +16,7 @@ import LocationsManTab from 'screens/Locations/Manage/Manage';
 import ProductsMain from 'screens/Products/Main/index';
 import ProductsAdd from 'screens/Products/Add/index';
 import ProductsPhone from 'screens/Products/Phone/index';
+import ProductsTablet from 'screens/Products/Tablet/index';
 
 const AuthRoute = (props) => (
   localStorage.getItem('token') !== null
@@ -61,7 +62,7 @@ class Routes extends Component {
               <AuthRoute exact path="/locations/manage/:store_id/:tab" component={LocationsManTab} />
               <AuthRoute exact path="/products/new" component={ProductsAdd} />
               <AuthRoute exact path="/products/manage" component={ProductsMain} />
-              <AuthRoute exact path="/products/manage/device/:type/:store_id" component={ProductsPhone} />
+              <AuthRoute exact path="/products/manage/device/phone/:store_id" component={ProductsPhone} />
             </Switch>
           </main>
         </div>
--- src/screens/Products/Phone/Info.js
+++ src/screens/Products/Phone/Info.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 // import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+
 
 // import AppBar from '@material-ui/core/AppBar';
 // import Tabs from '@material-ui/core/Tabs';
@@ -10,12 +11,37 @@ import PropTypes from 'prop-types';
 // import { getProducts } from 'redux/firebase/actions';
 // import { productsSelector } from 'redux/firebase/selectors';
 
-class Info extends Component {
+import Offer from 'components/Offer';
+import Colors from 'components/Colors';
+import Display from 'components/Display';
+import Camera from 'components/Camera';
+import Battery from 'components/Battery';
+import InputEvent from 'components/InputEvent';
+
+class Info extends InputEvent {
 	render() {
-		console.log(this.props.product);
+		const { product } = this.props;
+		console.log(product);
 		return (
-			<div id="products-man-phone-info" className="Container-box">
-				Info
+			<div id="products-man-phone-info">
+				<div className="info-container">
+					{this.renderGrid('white', <Offer offer={product.offer} />)}
+				</div>
+				<div className="info-container">
+					{this.renderGrid('white', product.description)}
+				</div>
+				<div className="info-container">
+					{this.renderGrid('white', <Colors colors={product.colors} />)}
+				</div>
+				<div className="info-container">
+					{this.renderGrid('white', <Display display={product.display} />)}
+				</div>
+				<div className="info-container">
+					{this.renderGrid('white', <Camera camera={product.camera} />)}
+				</div>
+				<div className="info-container">
+					{this.renderGrid('white', <Battery battery={product.battery} />)}
+				</div>
 			</div>
 		);
 	}
