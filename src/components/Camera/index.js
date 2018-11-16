f12f62bd4df7453f2543c9679f08d46eed6647cc
nativelocation <bluesky6460@mail.com>
2018-11-16 18:17:42 -0300

    update src/components bg, state, and logic

--- src/components/Camera/index.js
+++ src/components/Camera/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 import camerabg from 'assets/images/camera.png';
+import emptybg from 'assets/images/empty.png';
 
 class Camera extends Component {
 	render() {
@@ -9,7 +10,10 @@ class Camera extends Component {
 		return (
 			<div id="components-camera" className="component-camera-container">
 				<div className="content-box">
-					<img className="camera-bg" src={camerabg} alt="" />
+					<div className="bg-image">
+						<img className="camera-bg" src={camerabg} alt="" />
+						<img className="empty-bg" src={emptybg} alt="" />
+					</div>
 					<div className="camera-content-container">
 						<div className="camera-content">
 							<Grid container>
