e63ca934ee744fc9502aedbd847370e879601086
nativelocation <bluesky6460@mail.com>
2018-11-17 11:51:19 -0300

    update router

--- src/routes/index.js
+++ src/routes/index.js
@@ -63,6 +63,7 @@ class Routes extends Component {
               <AuthRoute exact path="/products/manage" component={ProductsMain} />
               <AuthRoute exact path="/products/manage/device/phone/:store_id" component={Product} />
               <AuthRoute exact path="/products/manage/device/tablet/:store_id" component={Product} />
+              <AuthRoute exact path="/products/manage/device/watch/:store_id" component={Product} />
             </Switch>
           </main>
         </div>
