a8e84e41438ef2fbcc7c0eaf2e55ee1f29504f7f
nativelocation <bluesky6460@mail.com>
2018-11-20 19:51:15 -0300

    update structure for service and device products

---
+++ src/screens/Products/Components/Services/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class Services extends Component {
+	render() {
+		return (
+			<div />
+		);
+	}
+}
+
+// Services.propTypes = {
+// };
+
+// Services.defaultProps = {
+// };
+
+export default Services;
