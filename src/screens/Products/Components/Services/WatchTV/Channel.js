99221286c7ab2a20874a1c4d399eaf3e6d00713a
sendevman <devman.sd@gmail.com>
2018-11-21 20:26:58 -0300

    Add channel component and assets

---
+++ src/screens/Products/Components/Services/WatchTV/Channel.js
@@ -0,0 +1,104 @@
+import ae from 'assets/images/channels/original/ae.jpg';
+import amc from 'assets/images/channels/original/amc.jpg';
+import animal_planet from 'assets/images/channels/original/animal-planet.jpg';
+import audience from 'assets/images/channels/original/audience.jpg';
+import bbc_world_news from 'assets/images/channels/original/bbc-world-news.jpg';
+import bbc from 'assets/images/channels/original/bbc.jpg';
+import bet from 'assets/images/channels/original/bet.jpg';
+import boomerang from 'assets/images/channels/original/boomerang.jpg';
+import cn from 'assets/images/channels/original/cn.jpg';
+import cnn from 'assets/images/channels/original/cnn.jpg';
+import comedy_central from 'assets/images/channels/original/comedy-central.jpg';
+import discovery from 'assets/images/channels/original/discovery.jpg';
+import food_network from 'assets/images/channels/original/food-network.jpg';
+import fyi from 'assets/images/channels/original/fyi.jpg';
+import hallmark from 'assets/images/channels/original/hallmark.jpg';
+import hgtv from 'assets/images/channels/original/hgtv.jpg';
+import history from 'assets/images/channels/original/history.jpg';
+import hln from 'assets/images/channels/original/hln.jpg';
+import ifc from 'assets/images/channels/original/ifc.jpg';
+import investigation_discovery from 'assets/images/channels/original/investigation-discovery.jpg';
+import lifetime from 'assets/images/channels/original/lifetime.jpg';
+import lifetime_movies from 'assets/images/channels/original/lifetime-movies.jpg';
+import mtv from 'assets/images/channels/original/mtv.jpg';
+import nicktoons from 'assets/images/channels/original/nicktoons.jpg';
+import own from 'assets/images/channels/original/own.jpg';
+import sundance from 'assets/images/channels/original/sundance.jpg';
+import tbs from 'assets/images/channels/original/tbs.jpg';
+import tcm from 'assets/images/channels/original/tcm.jpg';
+import teennick from 'assets/images/channels/original/teennick.jpg';
+import tlc from 'assets/images/channels/original/tlc.jpg';
+import tnt from 'assets/images/channels/original/tnt.jpg';
+import trutv from 'assets/images/channels/original/trutv.jpg';
+import velocity from 'assets/images/channels/original/velocity.jpg';
+import vh1 from 'assets/images/channels/original/vh1.jpg';
+import viceland from 'assets/images/channels/original/viceland.jpg';
+import wetv from 'assets/images/channels/original/wetv.jpg';
+
+
+export const Channel = () => (
+	<div id="watchtv-channel">
+		<div className="txtSaveDescription text-align-center">
+			Channel
+		</div>
+		<div className="d-flex justify-content-center align-items-center">
+			<img className="channel-img" src={ae} alt="" />
+			<img className="channel-img" src={amc} alt="" />
+			<img className="channel-img" src={animal_planet} alt="" />
+		</div>
+		<div className="d-flex justify-content-center align-items-center">
+			<img className="channel-img" src={audience} alt="" />
+			<img className="channel-img" src={bbc_world_news} alt="" />
+			<img className="channel-img" src={bbc} alt="" />
+		</div>
+		<div className="d-flex justify-content-center align-items-center">
+			<img className="channel-img" src={bet} alt="" />
+			<img className="channel-img" src={boomerang} alt="" />
+			<img className="channel-img" src={cn} alt="" />
+		</div>
+		<div className="d-flex justify-content-center align-items-center">
+			<img className="channel-img" src={cnn} alt="" />
+			<img className="channel-img" src={comedy_central} alt="" />
+			<img className="channel-img" src={discovery} alt="" />
+		</div>
+		<div className="d-flex justify-content-center align-items-center">
+			<img className="channel-img" src={food_network} alt="" />
+			<img className="channel-img" src={fyi} alt="" />
+			<img className="channel-img" src={hallmark} alt="" />
+		</div>
+		<div className="d-flex justify-content-center align-items-center">
+			<img className="channel-img" src={hgtv} alt="" />
+			<img className="channel-img" src={history} alt="" />
+			<img className="channel-img" src={hln} alt="" />
+		</div>
+		<div className="d-flex justify-content-center align-items-center">
+			<img className="channel-img" src={ifc} alt="" />
+			<img className="channel-img" src={investigation_discovery} alt="" />
+			<img className="channel-img" src={lifetime} alt="" />
+		</div>
+		<div className="d-flex justify-content-center align-items-center">
+			<img className="channel-img" src={lifetime_movies} alt="" />
+			<img className="channel-img" src={mtv} alt="" />
+			<img className="channel-img" src={nicktoons} alt="" />
+		</div>
+		<div className="d-flex justify-content-center align-items-center">
+			<img className="channel-img" src={own} alt="" />
+			<img className="channel-img" src={sundance} alt="" />
+			<img className="channel-img" src={tbs} alt="" />
+		</div>
+		<div className="d-flex justify-content-center align-items-center">
+			<img className="channel-img" src={tcm} alt="" />
+			<img className="channel-img" src={teennick} alt="" />
+			<img className="channel-img" src={tlc} alt="" />
+		</div>
+		<div className="d-flex justify-content-center align-items-center">
+			<img className="channel-img" src={tnt} alt="" />
+			<img className="channel-img" src={trutv} alt="" />
+			<img className="channel-img" src={velocity} alt="" />
+		</div>
+		<div className="d-flex justify-content-center align-items-center">
+			<img className="channel-img" src={vh1} alt="" />
+			<img className="channel-img" src={viceland} alt="" />
+			<img className="channel-img" src={wetv} alt="" />
+		</div>
+	</div>);
