cefcb15bbf63279a926de835eecb6e59ccdece0c
nativelocation <bluesky6460@mail.com>
2018-11-22 15:22:00 -0300

    Add reliability component

---
+++ src/screens/Products/Components/Services/DirectTV/DirectTVReliability.js
@@ -0,0 +1,15 @@
+import reliability from 'assets/images/reliability.png';
+
+const DirectTVReliability = () => (
+	<div id="directtv-reliability">
+		<img className="reliability-img" src={reliability} alt="" />
+		<div className="reliability-content-title">Get piece of mind with DIRECTV</div>
+		<div className="reliability-content">
+			Stop settling for cable. For the 18th year in a row, DIRECTV is rated higher in customer satisfaction than
+			Cable4. And with our 99% worry-free signal reliability, you get a TV experience you can depend on. Plus,
+			DIRECTV has been named as an ENERGY STAR® Partner of the Year for the 3rd year in a row.
+		</div>
+	</div>
+);
+
+export default DirectTVReliability;
