569fdb527efa65cb02dc471f2ad1866cc5c8b0c5
sendevman <devman.sd@gmail.com>
2018-11-22 20:13:21 -0300

    update reliability and slider directv component

--- src/screens/Products/Components/Services/DirectTV/index.js
+++ src/screens/Products/Components/Services/DirectTV/index.js
@@ -3,14 +3,22 @@ import PropTypes from 'prop-types';
 
 import Grid from '@material-ui/core/Grid';
 
+import DirectTVSlider from './DirectTVSlider';
+import DirectTVReliability from './DirectTVReliability';
 import InputEvent from 'components/InputEvent';
 
 class DirectTV extends InputEvent {
 	render() {
+		const { currentProduct } = this.props;
 		return (
 			<div id="products-services-directtv" className="Container-box">
 				<div className="default-inner-container">
+					<DirectTVSlider currentProduct={currentProduct} />
+					<div className="service-directv-txtTitle">Find the Package that is right for you</div>
+
+					<div className="service-directv-txtTitle">RELIABILITY</div>
 					<Grid container spacing={24}>
+						{this.renderGrid('white', <DirectTVReliability />)}
 					</Grid>
 				</div>
 			</div>
