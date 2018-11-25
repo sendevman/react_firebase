ac8ec0267092d163ea2d77671516dc023307d0ca
nativelocation <bluesky6460@mail.com>
2018-11-25 11:25:20 -0300

    update slider structure and add assets

--- src/screens/Products/Components/Services/DirectTVNow/DirectTVNowSlider.js
+++ src/screens/Products/Components/Services/DirectTVNow/DirectTVNowSlider.js
@@ -1,22 +1,72 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-import directvnow from '../../assets/images/files/directvnow1.png';
+import directvnow from 'assets/images/directvnow1.png';
 
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 class DirectTVNowSlider extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectedItem: 0,
+		};
+	}
+
+	handleOnChange = index => {
+		this.setState({ selectedItem: index });
+	}
+
+	renderBullets = () => (
+		<div>
+			<img className="title-img" src={directvnow} alt="" />
+			<div className="get-streaming">
+				<div className="get-streaming-title">
+					<span style={{ color: '#009FDB' }}>LET'S GET</span> STREAMING!
+				</div>
+
+			</div>
+		</div>
+	)
+
+	renderSubCards = () => {}
+
+	renderInternational = () => {}
+
 	render() {
+		const { currentProduct } = this.props;
+		const { selectedItem } = this.state;
 		return (
 			<div id="directv-now-get">
-				<img src={directvnow} alt="" />
-				<div className="get-streaming">
-					<div className="get-streaming-title">
-						<span style={{ color: '#00F' }}>LET'S GET</span> STREAMING!
-					</div>
-
-				</div>
+				<Carousel
+					centerMode
+					centerSlidePercentage={92.5}
+					emulateTouch
+					selectedItem={selectedItem}
+					showIndicators={false}
+					showThumbs={false}
+					showStatus={false}
+					showArrows={false}
+					onChange={this.handleOnChange}>
+					{currentProduct.carouselData.map((carouselData, index) => (
+						<div key={index}>
+							{carouselData.type === 'bullets' && this.renderBullets(carouselData)}
+							{carouselData.type === 'sub_cards' && this.renderSubCards(carouselData)}
+							{carouselData.type === 'international' && this.renderInternational(carouselData)}
+						</div>))}
+				</Carousel>
 			</div>
 		);
 	}
 }
+
+DirectTVNowSlider.propTypes = {
+	currentProduct: PropTypes.object,
+};
+
+DirectTVNowSlider.defaultProps = {
+	currentProduct: {},
+};
 
 export default DirectTVNowSlider;
