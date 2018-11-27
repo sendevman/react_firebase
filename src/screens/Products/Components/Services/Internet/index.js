6c1a48200bc36803aa3fc90b9f44e4dea7381111
nativelocation <bluesky6460@mail.com>
2018-11-27 19:40:39 -0300

    Add internet service component

---
+++ src/screens/Products/Components/Services/Internet/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// import Grid from '@material-ui/core/Grid';
+
+import InternetSlider from './InternetSlider';
+import InputEvent from 'components/InputEvent';
+
+class Internet extends InputEvent {
+	render() {
+		const { currentProduct } = this.props;
+		return (
+			<div id="products-services-internet" className="Container-box">
+				<div className="default-inner-container">
+					<InternetSlider currentProduct={currentProduct} />
+					<div className="learn-more">
+						To Learn more ask a sales associate
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+Internet.propTypes = {
+	currentProduct: PropTypes.object,
+};
+
+Internet.defaultProps = {
+	currentProduct: {},
+};
+
+export default Internet;
