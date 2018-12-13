e63a6ba7b78e9b9b684bc1a02b1db2ab4ac3141c
sendevman <devman.sd@gmail.com>
2018-12-12 21:47:48 -0300

    Add type props, Add cards components

--- src/screens/Products/Components/ServicesForm.js
+++ src/screens/Products/Components/ServicesForm.js
@@ -1,13 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import InputEvent from 'components/InputEvent';
 
-class ProductForm extends InputEvent {
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import Tooltip from '@material-ui/core/Tooltip';
+import Button from '@material-ui/core/Button';
+
+import AddIcon from '@material-ui/icons/Add';
+
+import Cards from './Cards';
+
+import { getCardTypes } from 'redux/firebase/actions';
+import { cardTypesSelector } from 'redux/firebase/selectors';
+
+class ProductForm extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
 			currentProduct: props.currentProduct,
+			cardType: '',
 		};
+	}
+
+	componentDidMount() {
+		const { cardTypes, getCardTypes } = this.props;
+		if (cardTypes.length === 0) {
+			getCardTypes();
+		}
 	}
 
 	componentWillReceiveProps(nextProps) {
@@ -16,22 +40,92 @@ class ProductForm extends InputEvent {
 		}
 	}
 
+	handleChange = (event) => {
+		this.setState({
+			cardType: event.target.value,
+		});
+	}
+
+	updateCards = (cards) => {
+		this.props.updateCurrentProduct(cards);
+	}
+
 	render() {
+		const { cardTypes, type } = this.props;
+		const { cardType, currentProduct } = this.state;
+
 		return (
-			<div />
+			<div>
+				<div style={{ marginBottom: '20px' }}>
+					<Tooltip title="Add Card" placement="bottom">
+						<div>
+							<Button
+								variant="contained"
+								size="medium"
+								aria-label="Add Card"
+								className="att-white margin-top margin-left"
+								onClick={this.handleOnClick}>
+								<AddIcon /> Card
+							</Button>
+						</div>
+					</Tooltip>
+				</div>
+
+				<FormControl variant="outlined" style={{ width: '75%', marginBottom: '20px' }}>
+					<InputLabel
+						ref={ref => {
+							this.InputLabelRef = ref;
+						}}
+						htmlFor="outlined-age-simple"
+					>
+						Card Type
+					</InputLabel>
+					<Select
+						value={cardType}
+						onChange={this.handleChange}
+						input={
+							<OutlinedInput
+								labelWidth={80}
+								name="age"
+								id="outlined-age-simple"
+							/>
+						}
+					>
+						<MenuItem value=""><em>None</em></MenuItem>
+						{cardTypes.map((item, index) => (
+							<MenuItem key={index} value={(index + 1) * 10}>{item.subType}</MenuItem>
+						))}
+					</Select>
+				</FormControl>
+
+				<Cards
+					type={type}
+					currentProduct={currentProduct}
+					updateCards={this.updateCards} />
+			</div>
 		);
 	}
 }
 
+const mapStateToProps = state => ({
+	cardTypes: cardTypesSelector(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+	getCardTypes: () => dispatch(getCardTypes()),
+});
+
 ProductForm.propTypes = {
+	type: PropTypes.string,
+	cardTypes: PropTypes.array.isRequired,
 	currentProduct: PropTypes.object,
+	getCardTypes: PropTypes.func.isRequired,
 	updateCurrentProduct: PropTypes.func.isRequired,
-	handleSave: PropTypes.func.isRequired,
-	handleCancel: PropTypes.func.isRequired,
 };
 
 ProductForm.defaultProps = {
+	type: 'edit',
 	currentProduct: {},
 };
 
-export default ProductForm;
+export default connect(mapStateToProps, mapDispatchToProps)(ProductForm);
