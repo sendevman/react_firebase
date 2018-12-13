89555ec231d13e3d2107b8b4e36e0e28ca263b0f
nativelocation <bluesky6460@mail.com>
2018-12-13 09:42:22 -0300

    Add cardView component

---
+++ src/components/CardView/index.js
@@ -0,0 +1,149 @@
+import PropTypes from 'prop-types';
+
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+
+import CloseIcon from '@material-ui/icons/Close';
+
+import InputEvent from 'components/InputEvent';
+
+class CardView extends InputEvent {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			body: props.data.body || '',
+			legal: props.data.legal || '',
+			title: props.data.title || '',
+			subtitle: props.data.subtitle || '',
+			imageNameHeroSrc: props.data.heroImg || '',
+			imgHeroSrc: '',
+			imgHeroSrcType: undefined,
+			changeState: false,
+		};
+	}
+
+	componentWillReceiveProps(nextProps) {
+		const { data } = this.props;
+		if (data !== nextProps.data) {
+			this.setState({
+				body: nextProps.data.body || '',
+				legal: nextProps.data.legal || '',
+				title: nextProps.data.title || '',
+				subtitle: nextProps.data.subtitle || '',
+				imageNameHeroSrc: nextProps.data.heroImg || '',
+			});
+		}
+	}
+
+	handleInputFileChange = (event) => {
+		const target = event.target;
+		const file = target.files[0];
+
+		if (file) {
+			this.setState({ });
+			const reader = new FileReader();
+			reader.onload = () => {
+				this.setState({
+					imgHeroSrc: reader.result,
+					imageNameHeroSrc: file.name,
+					imgHeroSrcType: file,
+					changeState: true,
+				}, () => this.handleSave());
+			};
+			reader.readAsDataURL(file);
+		}
+	}
+
+	handleInputChange = (e, type) => {
+		const stateCopy = Object.assign({}, this.state);
+		stateCopy[type] = e.target.value;
+		this.setState({
+			...stateCopy,
+			changeState: true,
+		}, () => this.handleSave());
+	};
+
+	handleSave = () => {
+		const { title, subtitle, imgHeroSrc, imgHeroSrcType, imageNameHeroSrc } = this.state;
+		const { index } = this.props;
+		const data = {
+			title,
+			subtitle,
+			heroImg: imageNameHeroSrc,
+		};
+		this.props.handleSave(data, imgHeroSrcType, imgHeroSrc, index);
+		this.setState({
+			changeState: false,
+		});
+	}
+
+	handleCancel = () => {
+		this.props.handleCancel();
+	}
+
+	render() {
+		const { title } = this.props;
+
+		return (
+			<Grid item xs={12}>
+				<div className="label-products-table select-text">{title}</div>
+
+				<Grid container spacing={24}>
+					<Grid item xs={12} sm={12}>
+						<FormGroup>
+							{this.renderText('title', 'Title')}
+							{this.renderText('subtitle', 'Subtitle')}
+							{this.renderText('body', 'Body')}
+							{this.renderText('legal', 'Legal')}
+						</FormGroup>
+
+						<div className="cardview-upload-box">
+							<input
+								id={`flat-button-file-hero-${title}`}
+								className="file-input"
+								accept="image/*"
+								type="file"
+								onChange={event => this.handleInputFileChange(event)} />
+
+							<label className="flat-button-file" htmlFor={`flat-button-file-hero-${title}`}>
+								<Button component="span" variant="contained" size="small" className="upload-button">
+									Upload
+								</Button>
+							</label>
+
+							<FormControl className="select-zone-box">
+								{this.renderText('imageNameHeroSrc', '', 'upload-text-field', 'Hero Image')}
+							</FormControl>
+						</div>
+					</Grid>
+				</Grid>
+				<div className="buttons-box">
+					{this.renderButton('Cancel', 'red', () => this.handleCancel(), <CloseIcon />)}
+				</div>
+
+			</Grid>
+		);
+	}
+}
+
+CardView.propTypes = {
+	index: PropTypes.number.isRequired,
+	type: PropTypes.string,
+	data: PropTypes.object,
+	title: PropTypes.string,
+	handleSave: PropTypes.func,
+	handleCancel: PropTypes.func,
+};
+
+CardView.defaultProps = {
+	type: 'edit',
+	data: {},
+	title: '',
+	handleSave: () => {},
+	handleCancel: () => {},
+};
+
+export default CardView;
